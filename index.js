944392ec5f3c0ed9ac82fffc3e4ee1382e872ff0
Laura Mejía Celis <lauram2celis@gmail.com>
2023-10-02 20:45:09 -0500

    sprint 2 dynamic load ok

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ const items = document.getElementById('items')
 const templateCard = document.getElementById('template-card').content
 const fragment = document.createDocumentFragment()
 
-/* Carga Dináamica de Elementos:
+/*✔ 4. Carga Dinámica de Elementos:
 Modificar la página de inicio (Home.html) para cargar dinámicamente los productos desde una estructura de datos en JavaScript, sin utilizar librerías o frameworks adicionales.
 Los productos deben incluir al menos una imagen, un nombre, una descripción y un precio.
 Utilizar eventos y funciones en JavaScript para lograr esta carga dinámica. */
@@ -10,7 +10,7 @@ Utilizar eventos y funciones en JavaScript para lograr esta carga dinámica. */
 // Eventos
 // El evento DOMContentLoaded es disparado cuando el documento HTML ha sido completamente cargado y parseado
 document.addEventListener('DOMContentLoaded', () => { fetchData() });
-//cards.addEventListener('click', e => { addCarrito(e) });
+items.addEventListener('click', e => { addToCart(e) });
 //items.addEventListener('click', e => { btnAumentarDisminuir(e) })
 
 // Traer productos
@@ -25,12 +25,19 @@ const fetchData = async () => {
 const createCards = data =>{
   data.forEach(item => {
     console.log(item)
+    templateCard.querySelector('img').setAttribute("src", item.image)
     templateCard.querySelector('h4').textContent = item.name
-    templateCard.querySelector('p').textContent = item.price
-    //templateCard.querySelector('button').dataset.id = item._id
+    templateCard.querySelector('h5').textContent = item.price
+    templateCard.querySelector('p').textContent = item.description
+    templateCard.querySelector('button').dataset.id = item.id
 
     const clone = templateCard.cloneNode(true)
     fragment.appendChild(clone)
   })
   items.appendChild(fragment)
+}
+
+//Agregar productos al carrito
+const addToCart = e => {
+  console.log(e.target)
 }
